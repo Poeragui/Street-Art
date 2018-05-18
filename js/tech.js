04c90fdfc17069b5057392c7cbe4c5ddd77c2d89
Poeragui <parai.tcc@gmail.com>
2018-05-18 03:32:43 -0700

    modification et finalise le site ou presque

---
+++ js/tech.js
@@ -0,0 +1,17 @@
+<!-- jQuery -->
+<script type="text/javascript" src="../libs/jquery/jquery.js"></script>
+
+<!-- SmartMenus jQuery plugin -->
+<script type="text/javascript" src="../jquery.smartmenus.js"></script>
+
+<!-- SmartMenus jQuery init -->
+<script type="text/javascript">
+	$(function() {
+		$('#main-menu').smartmenus({
+			mainMenuSubOffsetX: -1,
+			mainMenuSubOffsetY: 4,
+			subMenusSubOffsetX: 6,
+			subMenusSubOffsetY: -6
+		});
+	});
+</script>
